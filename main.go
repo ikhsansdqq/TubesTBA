1a3da8df9905975fbf606cf3e3a85189ab795810
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2023-06-09 08:09:11 +0700

    Initial commit
    - Type ./main to run the app
    - Just added input to read

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	// Create a new scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Prompt the user for input
+	fmt.Print("Enter your name: ")
+
+	// Read the input
+	scanner.Scan()
+	name := scanner.Text()
+
+	// Print the input
+	fmt.Printf("Hello, %s!\n", name)
 }
